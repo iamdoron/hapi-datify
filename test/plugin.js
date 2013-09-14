67c63dfa8610cd8408d79681c380811751d46cb2
doron pagot <doronpagot@gmail.com>
2013-09-14 15:05:00 +0300

    datify incoming payload

--- test/plugin.js
+++ test/plugin.js
@@ -1,5 +1,9 @@
 var Lab = require('lab');
 var Hapi = require('hapi');
+var Domain = require('domain');
+var Chai = require('chai');
+
+Chai.should();
 
 // Test shortcuts
 
@@ -10,6 +14,7 @@ var describe = Lab.experiment;
 var it = Lab.test;
 
 describe('hapi-datify', function () {
+
     it('can be added as a plugin to hapi', function (done) {
 
         var server = new Hapi.Server();
@@ -17,6 +22,27 @@ describe('hapi-datify', function () {
 
             expect(err).to.not.exist;
             done();
+        });
+    }),
+
+    it('datifies an incoming payload', function (done) {
+
+        var server = new Hapi.Server();
+        server.route({ method: 'POST', path: '/documents', handler: function(request){
+    		request.payload.should.eql({this: {is: {date: new Date('2011-09-13T17:09:30.909Z'), other: "dds"}}});
+    		request.reply("ok");
+    		done();
+        } });
+
+        server.pack.require('../', function (err) {
+        	server.inject({
+        		method: "POST",
+        		url: "/documents",
+        		payload: '{"this": {"is": {"date": "2011-09-13T17:09:30.909Z", "other": "dds"}}}'
+        	}, function(res){
+        		res.result.should.eql("ok");
+        		done();
+        	});
         });
     });
 });
